31a0edeaa68a7589811899640bb97653bcb3b863
GeorgeMoucht <giorgosmoucht@gmail.com>
2021-07-24 22:45:37 +0300

    Fix the Query selection in user'
    profile

--- app/src/main/java/com/example/amaltheaapplication/Profile.java
+++ app/src/main/java/com/example/amaltheaapplication/Profile.java
@@ -11,61 +11,75 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.FirestoreRegistrar;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
 public class Profile extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    ArrayList<User> userArrayList;
-    MyAdapter myAdapter;
-    FirebaseFirestore db;
-
-
-    FirebaseAuth fAuth;
     Button btnLogOut;
-    ProgressDialog progressDialog;
+    TextView dFirstName, dLastName, dEmail;
+
+    FirebaseFirestore db;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        //Just a message to the user while data are fetching.
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setCancelable(false);
-        progressDialog.setMessage("Fetching data from Cloud...");
-        progressDialog.show();
+        dFirstName= findViewById(R.id.tvFirstNamep);
+        dLastName = findViewById(R.id.tvLastNamep);
+        dEmail = findViewById(R.id.tvEmailp);
 
-        //Initilize all we need to process data in db, and print them.
-        recyclerView = findViewById(R.id.profileRecyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        db = FirebaseFirestore.getInstance();
-        userArrayList = new ArrayList<User>();
-        myAdapter = new MyAdapter(Profile.this,userArrayList);
-
-        recyclerView.setAdapter(myAdapter);
-
-        //Reatrive the data from db now.
-        EventChangeListener();
-
-        //Log-out the user.
-        fAuth = FirebaseAuth.getInstance();
         btnLogOut = findViewById(R.id.LogOutBtn);
 
+        db = FirebaseFirestore.getInstance();
+
+        //Append TextViews with logged in user data.
+        fetchdata();
 
 
+
+    }
+
+    //This is the function that Query Select Profile data of logged in user.    ~ yt: Firestore Query | Firestore LIKE query | MD-Jamal~
+    public void fetchdata() {
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null) {
+            String ma = user.getEmail();
+        }
+
+        db.collection("users")
+                .whereEqualTo("email",user.getEmail())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for(QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                            //Care! All fields my be exactly the same with db fields.
+                            dFirstName.append(" "+document.getString("firstName"));
+                            dLastName.append(""+document.getString("lastName"));
+                            dEmail.append(""+document.getString("email"));
+                        }
+                    }
+                });
     }
     //log out function.
     public void logout(View view) {
@@ -74,35 +88,7 @@ public class Profile extends AppCompatActivity {
         finish();
     }
 
-    private void EventChangeListener() {
-        db.collection("users").orderBy("firstName", Query.Direction.ASCENDING)
-                .addSnapshotListener(new EventListener<QuerySnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                        //https://www.youtube.com/watch?v=Az4gXQAP-a4
 
-                        if(error != null) {     //if something goes wrong with data send from cloud.
-                            if(progressDialog.isShowing()) {
-                                progressDialog.dismiss();
-                            }
-                            Log.e("FireStore Error",error.getMessage());
-                            return;
-                        }
-                        //if we get the data then:
-                        for(DocumentChange dc: value.getDocumentChanges()) {
-                            if(dc.getType() == DocumentChange.Type.ADDED) { //ADDED means that this function will run everytime something new is ADDED in db or fresh start of application.
-                                userArrayList.add(dc.getDocument().toObject(User.class)); //Put the data in the user class.
-                            }
-                        }
-                        myAdapter.notifyDataSetChanged();
-                        if(progressDialog.isShowing()) {
-                            progressDialog.dismiss();
-                        }
-
-                    }
-                });
-
-    }
 
 
 

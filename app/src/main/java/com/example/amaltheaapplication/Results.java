9c756a8dea1554d4ee2708a8008bf0a15bbb931d
GeorgeMoucht <giorgosmoucht@gmail.com>
2021-07-25 16:20:42 +0300

    Initialize SearchResults.java and ImportResults.java . Also create the redirect functions.

--- app/src/main/java/com/example/amaltheaapplication/Results.java
+++ app/src/main/java/com/example/amaltheaapplication/Results.java
@@ -2,7 +2,9 @@ package com.example.amaltheaapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Results extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class Results extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+    }
+
+
+
+
+
+    //Redirect in activity_import_results.xml
+    public void redirectToImportResults(View view) {
+        startActivity(new Intent(getApplicationContext(), importResults.class));
+        finish();
+    }
+    //Redirect in activity_import_results.xml
+    public void redirectToSearchResults(View view) {
+        startActivity(new Intent(getApplicationContext(), SearchResults.class));
+        finish();
     }
 }

5430d30ffc1b5eaef8f5b8653867f0ee264a92c7
GeorgeMoucht <giorgosmoucht@gmail.com>
2021-07-27 00:46:31 +0300

    Init basic functions in importResults.java

--- app/src/main/java/com/example/amaltheaapplication/importResults.java
+++ app/src/main/java/com/example/amaltheaapplication/importResults.java
@@ -1,15 +1,80 @@
 package com.example.amaltheaapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class importResults extends AppCompatActivity {
+
+    EditText tv;
+    Button btnImportCapResData;
+    FirebaseFirestore fStore;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import_results);
+
+        tv = findViewById(R.id.etCapsuleId);
+        btnImportCapResData = findViewById(R.id.btnCommitCapResultsToDB);
+
+        //Retrieve the data from EditText.
+        String capsuleId = tv.getText().toString();
+
+
+        //When user click the Import Btn, all this happens.
+        btnImportCapResData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImportNewCapResults(capsuleId);                                      //Import new Result to db.
+            }
+        });
+
+    }
+
+
+    //The function that interact with db to create our new Users Results.
+    public void ImportNewCapResults(String capsuleId) {
+
+        fAuth = FirebaseAuth.getInstance();
+
+        String userId = fAuth.getCurrentUser().getUid();
+
+        Map<String,Object> results = new HashMap<>();
+        results.put("AuthUserID", userId);
+        results.put("CapsuleID", capsuleId);
+        //We need somehow to put the List of results here. ===========================================================================================================
+
+        fStore.collection("results")
+                .add(results)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(importResults.this,"Sucessfull register in cloud",Toast.LENGTH_SHORT).show();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(importResults.this,"Failed register in cloud",Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
 
     }

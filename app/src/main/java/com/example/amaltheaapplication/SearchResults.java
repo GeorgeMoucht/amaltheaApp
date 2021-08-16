12346182c07df2d25945027203111050c87c912a
GeorgeMoucht <giorgosmoucht@gmail.com>
2021-08-16 13:23:04 +0300

    Scrollview made. I need to make the context now.

--- app/src/main/java/com/example/amaltheaapplication/SearchResults.java
+++ app/src/main/java/com/example/amaltheaapplication/SearchResults.java
@@ -3,6 +3,7 @@ package com.example.amaltheaapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SearchResults extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class SearchResults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+
+        TextView mTitleWindow = (TextView) findViewById(R.id.titleWindow);
+        TextView mMessageWindow = (TextView) findViewById(R.id.messageWindow);
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String someMessage = " This is some test message that I write for my ScrollView. ";
+
+        for(int i = 0; i < 100; i++) {
+            stringBuilder.append(someMessage);
+        }
+        mMessageWindow.setText(stringBuilder.toString());
+
     }
 }
